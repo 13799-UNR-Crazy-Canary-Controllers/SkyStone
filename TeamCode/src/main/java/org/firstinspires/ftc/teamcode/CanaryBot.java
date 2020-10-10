3ff991d70b204e58012c6afafb9173be794b0456
Tylee Kareck <tylee.l.kareck@gmail.com>
2020-10-09 17:12:23 -0700

    Deleted some comments about modifying variables that no longer exist.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CanaryBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CanaryBot.java
@@ -33,7 +33,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-​
+
 /**
  * This is NOT an opmode.
  *
@@ -50,56 +50,47 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
  */
-public class HardwarePushbot
+public class CanaryBot
 {
     /* Public OpMode members. */
-    public DcMotor  leftDrive   = null;
-    public DcMotor  rightDrive  = null;
     public DcMotor frontLeftDrive = null;
     public DcMotor frontRightDrive = null;
     public DcMotor backLeftDrive = null;
     public DcMotor backRightDrive = null;
-​
-    public DcMotor  leftArm     = null;
-    public Servo    leftClaw    = null;
-    public Servo    rightClaw   = null;
-​
-    public static final double MID_SERVO       =  0.5 ;
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
-​
+
+//    public DcMotor  leftArm     = null;
+//    public Servo    leftClaw    = null;
+//    public Servo    rightClaw   = null;
+
+//    public static final double MID_SERVO       =  0.5 ;
+//    public static final double ARM_UP_POWER    =  0.45 ;
+//    public static final double ARM_DOWN_POWER  = -0.45 ;
+
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
     private ElapsedTime period  = new ElapsedTime();
-​
+
     /* Constructor */
     public HardwarePushbot(){
-​
+
     }
-​
+
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
         // Save reference to Hardware map
         hwMap = ahwMap;
-​
+
         // Define and Initialize Motors
-//        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-//        rightDrive = hwMap.get(DcMotor.class, "right_drive");
-//        leftArm    = hwMap.get(DcMotor.class, "left_arm");
         frontLeftDrive = hwMap.get(DcMotor.class,"FL");
         frontRightDrive = hwMap.get(DcMotor.class,"FR");
         backLeftDrive = hwMap.get(DcMotor.class,"BL");
         backRightDrive = hwMap.get(DcMotor.class,"BR");
-//        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-//        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+//
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
         // Set all motors to zero power
-//        leftDrive.setPower(0);
-//        rightDrive.setPower(0);
-//        leftArm.setPower(0);
         frontLeftDrive.setPower(0);
         frontRightDrive.setPower(0);
         backLeftDrive.setPower(0);
@@ -109,7 +100,7 @@ public class HardwarePushbot
 //        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-​
+
         // Define and initialize ALL installed servos.
 //        leftClaw  = hwMap.get(Servo.class, "left_hand");
 //        rightClaw = hwMap.get(Servo.class, "right_hand");
@@ -117,5 +108,5 @@ public class HardwarePushbot
 //        rightClaw.setPosition(MID_SERVO);
     }
 }
-​
+
 
